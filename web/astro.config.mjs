788e6e9e6c48feb1991efec6a9246a5271b2985a
yamid-granda <vallenatomanizales@gmial.com>
2024-06-03 22:58:40 -0500

    chore: scss preprocessor config

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,28 @@
 import { defineConfig } from 'astro/config';
-
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url';
 import react from "@astrojs/react";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    resolve: {
+      alias: [
+        { find: '@/', replacement: path.resolve(__dirname, './') },
+      ],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/styles/index" as *;
+          `,
+        },
+      },
+    },
+  }
 });
