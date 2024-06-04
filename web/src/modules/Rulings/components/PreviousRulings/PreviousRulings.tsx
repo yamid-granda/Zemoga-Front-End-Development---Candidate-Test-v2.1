b7f8ed866ea7501fbd81cc1f14b46a5fe0795a39
yamid-granda <vallenatomanizales@gmial.com>
2024-06-04 14:54:46 -0500

    feat: PreviousRulings component, preview selector

--- web/src/modules/Rulings/components/PreviousRulings/PreviousRulings.tsx
+++ web/src/modules/Rulings/components/PreviousRulings/PreviousRulings.tsx
@@ -1,13 +1,32 @@
 import './PreviousRulings.scss'
-import type { FunctionComponent } from 'react'
+import { type FunctionComponent, useState } from 'react'
 import { Ruling } from '../Ruling/Ruling'
 import type { IPreviousRulingsProps } from './types'
+import { SingleSelect } from '@/components/SingleSelect/SingleSelect'
+import type { ISelectOption } from '@/components/SingleSelect/types'
+
+const viewOptions: ISelectOption[] = [
+  { value: 'list', label: 'List' },
+  { value: 'grid', label: 'Grid' },
+]
 
 export const PreviousRulings: FunctionComponent<IPreviousRulingsProps> = (props) => {
   const { celebrities } = props
+  const [view, setView] = useState(viewOptions[0].value)
 
   return (
     <div className="rt-previous-rulings">
+      <div className="rt-previous-rulings__head">
+        <h2 className='rt-previous-rulings__title'>Previous Rulings</h2>
+
+        <SingleSelect
+          name="view"
+          value={view}
+          onChange={setView}
+          options={viewOptions}
+        />
+      </div>
+
       <div className="rt-previous-rulings__items">
         {celebrities.map(celebrity => (
           <div
